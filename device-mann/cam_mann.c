1e7d420b5b85a2f49d0a7a7bd4083bd9a0f8ae77
Guilherme Pinto <guilherme4479@academico.ufs.br>
2023-09-24 20:39:50 -0300

    video formating

--- device-mann/cam_mann.c
+++ device-mann/cam_mann.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <fcntl.h>           //system file control
 #include <unistd.h>          //unix standard
 #include <sys/ioctl.h>       //ioctl *importante para utilização do v4l2*
 #include <linux/videodev2.h> // v4l2
 
-// definir tamanho da webcam:
+// definir tamanho da webcam
 #define WIDTH 640
 #define HEIGHT 480
-#define PIXEL_FORMAT V4L2_PIX_FMT_BGR24
+// definir formato de vídeo
+#define FRAME_FORMAT V4L2_PIX_FMT_YVU420
 
 int main()
 {
     // abrir o dispositivo virtual localizado em /dev/video0
-    int fd = open("/dev/video0", O_WRONLY);
+    int fd = open("/dev/video0", O_RDWR);
     if (fd == -1)
     {
         perror("Falha ao abrir Dispositivo Virtual de Webcam");
