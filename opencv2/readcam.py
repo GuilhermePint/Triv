2b44484fd48ddfa364c2c31bbca7fef13556ad74
Guilherme-Pinto-Almeida <109819841+Guilherme-Pinto-Almeida@users.noreply.github.com>
2023-09-26 20:27:26 -0300

    opencv python using

---
+++ opencv2/readcam.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import pygame
+
+# audio init
+pygame.mixer.init()
+volume = 0.1
+
+cap = cv2.VideoCapture("RGB.mp4")
+
+# video framerate
+fps = 24
+delay = 1000 // fps
+
+# image size
+width = 60
+height = 60
+
+# red -> brownnoise
+red_audio = False
+green_audio = False
+blue_audio = False
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        continue
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # limit to red
+    lower_red = np.array([0, 100, 100])
+    higher_red = np.array([10, 255, 255])
+
+    # create mask
+    mask = cv2.inRange(hsv_frame, lower_red, higher_red)
+
+    # read red pixels
+    red_pixels = cv2.countNonZero(mask)
+
+    if red_pixels > 1000 and not red_audio:
+        pygame.mixer.music.load("brownnoise.wav")
+        pygame.mixer.music.set_volume(volume)
+        pygame.mixer.music.play()
+
+        red_audio = True
+    elif red_pixels == 0:
+        red_audio = False
+        pygame.mixer.music.stop()
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(delay)
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
