1878dbb644f559793c6a74c3156b1c20d6e0d046
Guilherme Pinto <guilherme4479@academico.ufs.br>
2023-10-01 22:58:34 -0300

    hand motion + mouse moving

--- HandTracking/HandDetector.py
+++ HandTracking/HandDetector.py
@@ -96,10 +96,7 @@ class handDetector():
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
         if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (255, 255, 255), cv2.FILLED)
         length = math.hypot(x2 - x1, y2 - y1)
 
         return length, img, [x1, y1, x2, y2, cx, cy]
--- HandTracking/motion.py
+++ HandTracking/motion.py
@@ -20,8 +20,6 @@ keys = [
 
 #starting Device
 device = uinput.Device(keys)
-
-# Wait for the system to detect the new device
 time.sleep(1)
 
 def move_mouse(x : int, y : int):
@@ -30,6 +28,31 @@ def move_mouse(x : int, y : int):
     device.emit(uinput.REL_X, x//2)
     device.emit(uinput.REL_Y, y//2)
 
+def click(x : int, y : int, btn : int = 0, count : int = 1):
+    # Check each argument
+
+    if btn > 2 or btn < 0 or type(btn) != int:
+        raise ValueError('btn must be 0, 1 or 2')
+    if count > 3 or count < 1 or type(count) != int:
+        raise ValueError('count must be 1, 2 or 3')
+    
+    move_mouse(x, y)
+    
+    # Click the desired button
+    if btn == 0:
+        for i in range(count):
+            device.emit(uinput.BTN_LEFT, 1)
+            device.emit(uinput.BTN_LEFT, 0)
+    elif btn == 1:
+        for i in range(count):
+            time.sleep(0.3)
+            device.emit(uinput.BTN_MIDDLE, 1)
+            device.emit(uinput.BTN_MIDDLE, 0)
+    elif btn == 2:
+        for i in range(count):
+            time.sleep(0.3)
+            device.emit(uinput.BTN_RIGHT, 1)
+            device.emit(uinput.BTN_RIGHT, 0)
 
 def get_screen_resolution():
     display = Xlib.display.Display()
@@ -41,7 +64,7 @@ def get_screen_resolution():
 screen_width, screen_height = get_screen_resolution()
 print(f"Screen Resolution: {screen_width}x{screen_height}")
 
-width, height = 640, 480
+width, height = 800, 600
 
 cap = cv2.VideoCapture(0)
 cap.set(3,width)
@@ -72,8 +95,18 @@ while True:
         if fingers[1] == 1 and fingers[0] == 0:
             x3 = np.interp(x1,(0,width),(0,screen_width))
             y3 = np.interp(y1,(0,height),(0,screen_height))
-            move_mouse(x1,y1)
+
+            print(x1,y1)
+            print(x3,y3)
+            #move_mouse(x3,y3)
+            move_mouse(int(x3),int(y3))
             time.sleep(0.02)
+
+        if fingers[1] == 1 and fingers[0] == 1:
+            lenght, img, linePoint = detector.findDistance(8,4,img,r=5)
+            if lenght>60:
+                cv2.rectangle(img,(linePoint[4]-10,linePoint[5]-10),(linePoint[4]+10,linePoint[5]+10),(255,255,255),2)
+                            
 
 
         
