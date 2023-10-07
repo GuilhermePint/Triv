1bed050b481eef1cf30b022eab01357143a3f5ae
Guilherme Pinto <guilherme4479@academico.ufs.br>
2023-10-07 16:00:01 -0300

    ajustes finais-1

--- motion.py
+++ motion.py
@@ -79,7 +79,7 @@ def main():
                     five_finger_timer = 0
                 
                 #tip Up, move Mode
-                if fingers[1] == 1 and fingers[0] == 0:
+                if fingers[1] == 1 and fingers[0] == 0 and fingers[2] == 0:
                     print('indicador')
                     cv2.rectangle(img,(reductionFrame,reductionFrame),(width-reductionFrame,height-reductionFrame),(255,255,255), 2)
                     x3 = np.interp(x1,(reductionFrame,width-reductionFrame),(0,screen_width))
@@ -99,12 +99,12 @@ def main():
                     if lenght>110:
                         cv2.rectangle(img,(linePoint[4]-10,linePoint[5]-10),(linePoint[4]+10,linePoint[5]+10),(255,255,255),2)
                         
-                        device.emit(uinput.BTN_RIGHT,1)
+                        device.emit(uinput.BTN_LEFT,1)
                     
-                    device.emit(uinput.BTN_RIGHT, 0)       
+                    device.emit(uinput.BTN_LEFT, 0)       
                     time.sleep(0.02)
 
-                if fingers[1] == 1 and fingers[2] == 1:
+                if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 0:
                     cv2.rectangle(img,(reductionFrame,reductionFrame),(width-reductionFrame,height-reductionFrame),(255,255,255), 2)
                     lenght, img, linePoint = detector.findDistance(12,8,img,r=5)
                     print(lenght)
